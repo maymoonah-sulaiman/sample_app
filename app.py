363ea7e6d94df60b5b00a3a82f7aea894b557de3
maymoonah-sulaiman <59207799+maymoonah-sulaiman@users.noreply.github.com>
2020-06-30 17:21:52 +0300

    testing deployment on heroku

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def create_app(test_config=None):
     @app.route('/')
     def get_greeting():
         excited = os.environ['EXCITED']
-        greeting = "Hello" 
+        greeting = "Hello first app good" 
         if excited == 'true': greeting = greeting + "!!!!!"
         return greeting
 
